0ffeb027aeec0093c4d9c0ef26f5fc932671b179
Ronaldo Hoch <ronaldopintohoch@gmail.com>
2017-10-08 03:10:29 -0300

    troca de estrutura node

--- site/server.js
+++ site/server.js
@@ -1,5 +1,5 @@
 var http = require("http"),
-	app = require("./config/express")();
+	app = require("./server/config/express")();
 
 http.createServer(app).listen(app.get("port"),function(){
 	console.log("ImperiumLARP em: "+app.get("port"));
